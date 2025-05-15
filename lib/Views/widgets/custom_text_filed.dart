429d8e3df6bd74aeeb4e369585685548dd3a22eb
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2025-05-15 15:28:57 +0300

    create custom text filed

---
+++ lib/Views/widgets/custom_text_filed.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  const CustomTextFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText:'Title ',
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor)
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(
+        color:color??  Colors.white
+      )
+      );
+  }
+}
